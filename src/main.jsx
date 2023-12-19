32571420c03bcc84ad45e5f0bfe85472a0756793
Abhisek <abhisekmaiti19@gmail.com>
2023-12-19 20:40:34 +0530

    jj

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -41,9 +40,7 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
