64304b35032939090d636dcf8f3ae0ad91f01676
Abhisek <abhisekmaiti19@gmail.com>
2023-12-20 14:26:09 +0530

    done

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,8 +2,26 @@
 import SidebarButton from "../sideBarButton/SidebarButton.jsx";
 import logo from "../../assets/logo.png";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import DB from "../../appwrite/services/db";
 
 export default function Sidebar() {
+  const slector = useSelector((state) => state.auth.status);
+  //retrive user details
+  const [userData, setUserData] = useState("");
+
+  useEffect(() => {
+    if (slector) {
+      DB.getUser(JSON.parse(localStorage.user).phone.slice(3)).then((data) => {
+        if (data.isAdmin) {
+          setUserData(data.isAdmin);
+        }
+      });
+    }
+  }, [slector]);
+
+  // kichu ekta
   const buttonDetails = [
     {
       text: "Home",
@@ -17,13 +35,14 @@ export default function Sidebar() {
       text: "Courses",
       where: `courses`,
     },
-    {
-      text: "Create",
-      where: `createjob`,
-    },
   ];
 
-  useEffect(() => {}, []);
+  if (userData === true) {
+    buttonDetails.push({
+      text: "Create",
+      where: "createjob",
+    });
+  }
 
   return (
     <div className="right-0 h-screen w-fit px-10 py-5 flex flex-col gap-5 shadow-md shadow-blue-100">
